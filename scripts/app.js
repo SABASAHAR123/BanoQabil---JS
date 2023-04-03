f2b38d04c6067d278e4db8fe68a3c71e24a70b30
Saba Sahar <saba.aafia@gmail.com>
2023-04-03 15:01:25 +0500

    first class

---
+++ scripts/app.js
@@ -0,0 +1,26 @@
+// DAY 1 - 3 APR 
+
+alert('Hello')
+document.write(" Sarey Pagal hain")
+console.log('Ji Sahi Kaha')
+
+var name = 'Usama'
+var fname = 'Usman'
+var num = 123
+
+console.log(name + " " + fname + " " + num)
+console.log(num)
+var name = prompt("Your Name Please.", 'Usama')
+document.write("Hello " + name)
+
+console.log(2+3)
+console.log(2-3)
+console.log(2*3)
+console.log(2/3)
+console.log(2**3)
+var num1 = +prompt('Number 1')
+var num2 = +prompt('Number 2')
+
+
+
+
